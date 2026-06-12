4ac7e77bfb9a3f1b367a4de56b533a64bad63ac2
Karen Chung <karenc@nvidia.com>
2026-06-12 15:57:13 -0700

    sweeping gate stuff

---
+++ recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_gate_sweep/run.sh
@@ -0,0 +1,2 @@
+srtctl apply -f /lustre/fsw/coreai_dlfw_dev/karenc/srt-slurm/recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_gate_sweep/gate_nooffload_tep4x2p_tep2x3d.yaml
+srtctl apply -f /lustre/fsw/coreai_dlfw_dev/karenc/srt-slurm/recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_gate_sweep/gate_nooffload_tep4x2p_tep4x3d.yaml
---
+++ recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_rebalance/run.sh
@@ -0,0 +1,2 @@
+srtctl apply -f /lustre/fsw/coreai_dlfw_dev/karenc/srt-slurm/recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_rebalance/rebal_tep2x2p_tep2x5d.yaml
+srtctl apply -f /lustre/fsw/coreai_dlfw_dev/karenc/srt-slurm/recipes/v1_isl_cost/cc-traces-weka-with-subagents-052726-256k/disagg_condp_rebalance/rebal_tep4x1p_tep2x5d.yaml
